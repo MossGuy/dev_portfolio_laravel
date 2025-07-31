19fd4db395d7563af8e6cd959ce44f75dea6e526
MossGuy <milanpallas@hotmail.nl>
2025-07-31 11:56:52 +0200

    tweaks hier en daar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('main')
     <section class="kader my-5 rounded-lg flex flex-col md:flex-row justify-evenly items-center">
-        <img src="{{ asset('images/ik.jpg') }}" class="w-100 md:w-50 rounded-lg mt-2 mb-15 border border-3" alt="Foto van mijzelf"/>
-        <ul class=" text-lg font-semibold p-4">
+        <img src="{{ asset('images/ik.jpg') }}" class="w-[280px] md:w-[200px] h-70 object-cover object-[center_70%] rounded-lg mt-2 mb-1 md:mb-14 border border-3" alt="Foto van mijzelf"/>
+        <ul class=" text-lg font-semibold p-4 md:mb-1 mb-10">
             <li class="text-2xl font-semibold">Ik ben:</li>
             <li>
                 <i class="bi bi-code-slash"></i>
@@ -31,4 +31,5 @@
 
     <section class="flex flex-col items-center justify-center h-full my-2 p-4 rounded-lg shadow-lg">
         <p>sectie met programmeer talen / frameworks die ik ken</p>
+    </section>
 @endsection
