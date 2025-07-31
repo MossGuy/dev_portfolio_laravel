f077bd60e4ef177edc4cd8f8f4858c6501f42b95
MossGuy <milanpallas@hotmail.nl>
2025-07-31 12:08:05 +0200

    tweaks hier en daar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
     <section class="kader my-5 rounded-lg flex flex-col md:flex-row justify-evenly items-center">
-        <img src="{{ asset('images/ik.jpg') }}" class="w-[280px] md:w-[200px] h-70 object-cover object-[center_70%] rounded-lg mt-2 mb-1 md:mb-14 border border-3" alt="Foto van mijzelf"/>
+        <img src="{{ asset('images/ik.jpg') }}" class="w-[280px] md:w-[200px] h-70 object-cover object-[center_70%] rounded-xl mt-2 mb-1 md:mb-14 border border-3" alt="Foto van mijzelf"/>
         <ul class=" text-lg font-semibold p-4 md:mb-1 mb-10">
             <li class="text-2xl font-semibold">Ik ben:</li>
             <li>
