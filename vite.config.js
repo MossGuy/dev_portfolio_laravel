9c7cbcdcd3e2f14c7df067874bf8bf232122271b
Milan <milanpallas@hotmail.nl>
2025-09-10 10:12:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+        manifest: true,          // zorg dat manifest.json in public/build staat
+        emptyOutDir: true,       // verwijdert oude bestanden bij nieuwe build
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -10,9 +21,4 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    build: {
-        outDir: 'public/build',   // <--- belangrijk!
-        manifest: true,           // maakt manifest.json aan
-        emptyOutDir: true,        // opruimen oude assets
-    },
 });
