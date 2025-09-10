4e3f3d4c092a8abf90df376b96aa0fa975ae6a84
Milan <milanpallas@hotmail.nl>
2025-09-10 09:34:39 +0200

    Update vite.config.js
    refresh true + build sectie

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
         }),
         tailwindcss(),
     ],
+    build: {
+        outDir: 'public/build',   // <--- belangrijk!
+        manifest: true,           // maakt manifest.json aan
+        emptyOutDir: true,        // opruimen oude assets
+    },
 });
