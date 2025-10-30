b4175fce4f62cf203a2decb8e92149776ff32c2d
MossGuy <milanpallas@hotmail.nl>
2025-10-30 15:04:52 +0100

    nieuwe build files

--- public/build/assets/home-B2Bx9ehL.js
+++ public/build/assets/home-B2Bx9ehL.js
@@ -1,4 +1,4 @@
-const b={html5:{title:"HTML5",description:`Ik kan een nette HTML-structuur aanhouden door middel van de elementen nav, main, footer en section. Ik ben bekend met de basiselementen zoals linkjes, afbeeldingen, formulieren en tabellen. Ook ben ik bekend met favicon-icoontjes. 
+const u={html5:{title:"HTML5",description:`Ik kan een nette HTML-structuur aanhouden door middel van de elementen nav, main, footer en section. Ik ben bekend met de basiselementen zoals linkjes, afbeeldingen, formulieren en tabellen. Ook ben ik bekend met favicon-icoontjes. 
      
     Als ik een webpagina ga maken, denk ik altijd zorgvuldig na over de layout volgens het mobile-first principe.`,images:["HTML5.png"]},cssFramework:{title:"CSS + Frameworks",description:`Ik gebruik nog vaak CSS om basiskleurenthema's op te slaan in de root-variabelen en zo een huisstijl te maken die makkelijk en snel gebruikt kan worden door alle pagina's in mijn project. 
         Ook ben ik bekend met flexbox, grids en media queries. 
@@ -12,17 +12,18 @@ const b={html5:{title:"HTML5",description:`Ik kan een nette HTML-structuur aanho
         Ik vind de Blade-templates erg fijn om mee te werken en zo views overzichtelijk op te bouwen door middel van layouts.  
         Ik kan werken met basisroutes, formulieren en de ingebouwde databasestructuur.  
         Verder ben ik bekend met middleware, de cache en logfuncties.`,images:["Laravel.png"]},Csharp:{title:"C#",description:`Naast de basis syntax heb ik C# gebruikt om kennis te maken met objectgeoriënteerd programmeren. 
-      `,images:["Csharp.png"]},AspDotNet:{title:"ASP.NET",description:"Ik wijger om die bloat ooit nog aan te raken al geef je mij een duidelijke handleiding",images:["AspDotNet.png"]}};document.addEventListener("DOMContentLoaded",()=>{const r=document.querySelectorAll(".skill"),t=document.getElementById("ervaring_body"),o=t.innerHTML,l=window.assetPath||"/images";r.forEach(n=>{n.addEventListener("click",()=>{const d=n.dataset.skill,e=b[d];if(e){const m=e.title==="ASP.NET",i="p-6 mt-6 animate-fadeIn max-w-5xl mx-auto flex flex-col items-center justify-center gap-8",a=`
+      `,images:["Csharp.png"]},AspDotNet:{title:"ASP.NET",description:"Ik wijger om die bloat ooit nog aan te raken al geef je mij een duidelijke handleiding",images:["AspDotNet.png"]}};document.addEventListener("DOMContentLoaded",()=>{const o=document.querySelectorAll(".skill"),t=document.getElementById("ervaring_body"),l=t.innerHTML,d=window.assetPath||"/images";o.forEach(n=>{n.addEventListener("click",()=>{const m=n.dataset.skill,e=u[m];if(e){const c=e.title==="ASP.NET",i="p-6 mt-6 animate-fadeIn max-w-5xl mx-auto flex flex-col items-center justify-center gap-8",a=`
           <button id="backButton"
             class="mx-auto w-fit items-center gap-2 px-5 py-2 border-2 border-[var(--primary)]
             bg-[var(--background)] hover:bg-[var(--highlight)] text-[var(--primary)]
             hover:text-white rounded-lg font-semibold transition duration-200">
             <i class="bi bi-x-lg"></i> Sluiten
           </button>
-        `,s=(e.images??[]).map((g,k)=>`
-              <img src="${l}/${g}" alt="${e.title} afbeelding ${k+1}"
-                class="w-24 h-24 lg:w-32 lg:h-32 object-contain mx-auto lg:mx-0">
-            `).join(""),c=m?`
+        `,s=(e.images??[]).map((k,r)=>{const b=r===0?"w-40 h-40 lg:w-48 lg:h-48":"w-20 h-20 lg:w-28 lg:h-28";return`
+        <img src="${d}/${k}" 
+            alt="${e.title} afbeelding ${r+1}"
+            class="${b} object-contain mx-auto lg:mx-0 transition-transform duration-300 hover:scale-105">
+      `}).join(""),g=c?`
               <div class="${i}">
                 ${s}
                 ${a}
@@ -45,4 +46,4 @@ const b={html5:{title:"HTML5",description:`Ik kan een nette HTML-structuur aanho
                   ${a}
                 </div>
               </div>
-            `;t.innerHTML=c,document.getElementById("backButton").addEventListener("click",()=>{t.innerHTML=o,document.dispatchEvent(new Event("DOMContentLoaded"))})}})})});
+            `;t.innerHTML=g,document.getElementById("backButton").addEventListener("click",()=>{t.innerHTML=l,document.dispatchEvent(new Event("DOMContentLoaded"))})}})})});
