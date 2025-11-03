1b23579202789c8b10c90e6e64aa90ed62a123f8
MossGuy <milanpallas@hotmail.nl>
2025-11-03 12:29:54 +0100

    hover style update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,7 @@
     <section class="my-2 p-4 rounded-lg shadow-lg text-center" id="ervaring_body">
         <h3 class="text-xl font-medium">Ervaring met talen en frameworks</h3>
         <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-            <li data-skill="html5" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="html5" class="skill font-semibold flex flex-col cursor-pointer transition rounded-lg p-1 hover:bg-[var(--highlight)] hover:shadow-lg">
                 HTML
                 <span>
                     <i class="bi bi-star-fill"></i>
@@ -45,7 +45,7 @@
                     <i class="bi bi-star"></i>
                 </span>
             </li>
-            <li data-skill="cssFramework" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="cssFramework" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition hover:bg-[var(--highlight)] rounded-lg p-1 hover:shadow-lg">
                 CSS / Frameworks
                 <span> <i class="bi bi-star-fill"></i>
                     <i class="bi bi-star-fill"></i>
@@ -54,7 +54,7 @@
                     <i class="bi bi-star"></i>
                 </span>
             </li>
-            <li data-skill="javascript" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="javascript" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition hover:bg-[var(--highlight)] rounded-lg p-1 hover:shadow-lg">
                 JavaScript
                 <span> <i class="bi bi-star-fill"></i>
                     <i class="bi bi-star-fill"></i>
@@ -63,7 +63,7 @@
                     <i class="bi bi-star"></i>
                 </span>
             </li>
-            <li data-skill="php" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="php" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition hover:bg-[var(--highlight)] rounded-lg p-1 hover:shadow-lg">
                 PHP
                 <span> <i class="bi bi-star-fill"></i>
                     <i class="bi bi-star-fill"></i>
@@ -72,7 +72,7 @@
                     <i class="bi bi-star"></i>
                 </span>
             </li>
-            <li data-skill="sql" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="sql" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition hover:bg-[var(--highlight)] rounded-lg p-1 hover:shadow-lg">
                 SQL
                 <span> <i class="bi bi-star-fill"></i>
                     <i class="bi bi-star-fill"></i>
@@ -81,7 +81,7 @@
                     <i class="bi bi-star"></i>
                 </span>
             </li>
-            <li data-skill="laravel" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="laravel" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition hover:bg-[var(--highlight)] rounded-lg p-1 hover:shadow-lg">
                 <span>
                     <i class="bi bi-stars highlight"></i>
                     Laravel
@@ -94,7 +94,7 @@
                     <i class="bi bi-star"></i>
                 </span>
             </li>
-            <li data-skill="Csharp" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="Csharp" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition hover:bg-[var(--highlight)] rounded-lg p-1 hover:shadow-lg">
                 C#
                 <span> 
                     <i class="bi bi-star-fill"></i>
@@ -103,7 +103,7 @@
                     <i class="bi bi-star"></i>
                 </span>
             </li>
-            <li data-skill="AspDotNet" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition border-[var(--secondary)] hover:border-[var(--primary)] border-2 rounded-lg p-1">
+            <li data-skill="AspDotNet" class="skill font-semibold flex flex-col cursor-pointer hover:text-blue-500 transition hover:bg-[var(--highlight)] rounded-lg p-1 hover:shadow-lg">
                 ASP.NET
                 <span>
                     <i class="bi bi-star-fill"></i> 
